af765d43112ff6f3f0a05efd58d35eb4cc2328dc
zaineb1802 <zainebdarchem@gmail.com>
2025-12-15 20:17:45 +0100

    Comment
    Comment out the import statement for seaborn.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
